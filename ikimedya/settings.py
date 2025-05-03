698a5ce6cbbfaa4117a7df0012678e2dc3296909
kazimshn <kazimsahinn01@gmail.com>
2025-05-04 00:09:36 +0300

    statikler eklendi

--- ikimedya/settings.py
+++ ikimedya/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",  # Statik dosyalar için
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
